31d1bbe2ebed65dc94f6139ea1308e60c35cc05e
Aritra Bhattacharya <aritra1235@gmail.com>
2024-09-02 01:50:35 +0530

    added permission and showing location

--- GeoSpot/ContentView.swift
+++ GeoSpot/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var locationManager = LocationManager()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if let location = locationManager.location {
+                Text("Latitude: \(location.coordinate.latitude), Longitude: \(location.coordinate.longitude)")
+                    .padding()
+            } else {
+                Text("Fetching location...")
+                    .padding()
+            }
+        }
+        .onAppear {
+            // This ensures the location updates when the view appears
+            locationManager.objectWillChange.sink { _ in
+                // Optional: Handle any additional logic when the location changes
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
--- GeoSpot/LocationManager.swift
+++ GeoSpot/LocationManager.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import CoreLocation
+import Combine
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
+    @Published var location: CLLocation?
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization() // Request permission
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let newLocation = locations.last else { return }
+        self.location = newLocation
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get location: \(error.localizedDescription)")
+    }
+}
+
